2fcdec095fb087e2c3761a44f1cf8f03fab2b792
Andrej <scooty-az@azet.sk>
2018-08-23 22:37:04 +0200

    first working server

---
+++ server.js
@@ -0,0 +1,12 @@
+const io = require('socket.io')();
+
+io.on('connection', socket => {
+    socket.on('echo', (...data) => {
+        socket.emit('echo', ...data);
+    });
+    socket.on('event', (event, ...data) => {
+        socket.emit(event, ...data);
+    });
+});
+
+io.listen(3000);
